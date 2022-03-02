4253c7161eb64b0939d6999ac47f083689bc1b54
Mars Cheung <mars.kwong.cheung@gmail.com>
2022-03-02 09:52:16 -0800

    implement catch-all for web urls to deal with page refresh 404 errors

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::view('/{path?}', 'app');
+// Route::view('/{path?}', 'app');
+Route::get('{path?}', function () {
+    return view('app'); // your start view
+})->where('path', '^((?!api).)*$'); // except 'api' word
