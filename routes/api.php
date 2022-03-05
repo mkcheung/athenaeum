d22c995bd2fbb3f0973c92cb54504a60e9de9bda
Mars Cheung <mars.kwong.cheung@gmail.com>
2022-03-05 13:30:07 -0800

    remove former api controller and unused routes from previous project iteration

--- routes/api.php
+++ routes/api.php
@@ -35,24 +35,3 @@ Route::group(['middleware' => 'api'], function($router) {
     Route::get('/books/showUserBooks', [BookController::class, 'showUserBooks']);
     Route::resource('books', BookController::class);
 });
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::group(['middleware' => ['jwt.verify']], function() {
-    Route::get('logout', [ApiController::class, 'logout']);
-    Route::get('get_user', [ApiController::class, 'get_user']);
-    Route::get('posts', [PostController::class, 'index']);
-    Route::get('posts/{id}', [PostController::class, 'show']);
-    Route::post('create', [PostController::class, 'store']);
-    Route::put('update/{post}',  [PostController::class, 'update']);
-    Route::delete('delete/{post}',  [PostController::class, 'destroy']);
-});
